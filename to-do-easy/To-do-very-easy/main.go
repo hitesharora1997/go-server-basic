f4af9c1d8f376d2b651c2494a5e67b8917ebba25
Hitesh Arora <hitesh.arora1997@gmail.com>
2023-08-29 15:23:02 +0200

    new changes

--- to-do-easy/To-do-very-easy/main.go
+++ to-do-easy/To-do-very-easy/main.go
@@ -1,9 +1,33 @@
 package main
 
 import (
+	"time"
+
 	"github.com/thedevsaddam/renderer"
 	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
 )
 
 var rnd *renderer.Render
 var db *mgo.Database // mongo db
+
+// My custom struct
+const (
+	hostname       string = "localhost:27017"
+	dbName         string = "demo_todo"
+	collectionName string = "todo"
+	port           string = "9000"
+)
+
+type ( // meed to look on my own
+	todoModel struct {
+		ID        bson.ObjectId `"bson:_id,omitempty"` // use of back ticks here
+		Title     string        `"bson: title"`
+		Completed bool          `"bson:completed"`
+		CreatedAt time.Time     `"bson:createdAt"`
+	}
+)
+
+func main() {
+	println("hello")
+}
