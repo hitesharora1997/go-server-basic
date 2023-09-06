81404ec993756b2c4cbc96abb0739f349a3047f6
Hitesh Arora <hitesh.arora1997@gmail.com>
2023-09-06 13:47:51 +0200

    new

--- go-server-basic/main.go
+++ go-server-basic/main.go
@@ -1,9 +1,35 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func helloHandler(w http.ResponseWriter, r http.Request) {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 not found", http.StatusNotFound)
+		return
+	}
+
+	if r.Method != "GET" {
+		http.Error(w, "method is not supported", http.StatusNotFound)
+		return
+	}
+
+	fmt.Fprintf(w, "hello!")
+}
 
 func main() {
 	fileserver := http.FileServer(http.Dir("./static")) // telling the golang to check the in the static folder
 	http.Handle("/", fileserver)                        // setting up a route for "/" and pointing it at our file server
+	http.HandleFunc("/form", formHandler)
+	http.HandleFunc("/hello", helloHandler)
+
+	fmt.Println("Starting the server at port 8080\n")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+
+	}
 
 }
