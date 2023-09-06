35c4e4f52f652a8fc9e9ef778263b0c9c8f0a31f
Hitesh Arora <hitesh.arora1997@gmail.com>
2023-09-06 12:02:33 +0200

    New

--- go-server-basic/main.go
+++ go-server-basic/main.go
@@ -3,5 +3,7 @@ package main
 import "net/http"
 
 func main() {
-	fileserver := http.FileServer(http.Dir("./static"))
+	fileserver := http.FileServer(http.Dir("./static")) // telling the golang to check the in the static folder
+	http.Handle("/", fileserver)                        // setting up a route for "/" and pointing it at our file server
+
 }
